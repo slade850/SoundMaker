e900ec22d5e964c4ece332b8898a35d52b96fb9e
Julien <julien.bardin81@gmail.com>
2019-05-16 10:38:33 +0200

    maj10

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ addEventListener('load',main);
 function showInfo() {
   removeListeners();
   
-  var modal = this.parentNode.getElementsByClassName('modal');
+  var modal = this.parentNode.getElementsByClassName('modal')[0];
       modal.style.display = 'block';
-  var close = modal.getElementsByClassName('close');
+  var close = modal.getElementsByClassName('close')[0];
       close.addEventListener('click',hideInfo);
   document.getElementById('overlay').addEventListener('click',hideInfo);
 }
