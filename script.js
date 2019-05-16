8535b0a89b963be892b7322a817a4d0e41835c00
Julien <julien.bardin81@gmail.com>
2019-05-16 15:36:34 +0200

    maj14

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ function main() {
   addListeners();
 }
 addEventListener('load',main);
-
-function showInfo() {
+var clique = document.getElementsByClassName('card');
+function showInfo(x) {
   removeListeners();
-  
-  var modal = this.parentNode.getElementsByClassName('modal')[0];
+
+  var modal = document.getElementsByClassName('modal')[x];
       modal.style.display = 'block';
   var close = modal.getElementsByClassName('close')[0];
       close.addEventListener('click',hideInfo);
   document.getElementById('overlay').addEventListener('click',hideInfo);
+  tur = 1;
 }
 
 function hideInfo() {
@@ -19,6 +20,7 @@ function hideInfo() {
     boxes[i].style.display = 'none';
   }
   addListeners();
+  stp();
 }
 
 function addListeners() {
@@ -41,6 +43,13 @@ function handleOverlay(status) {
   var overlay = document.getElementById('overlay');
       overlay.style.display = status;
 }
-  function lect(){
-  document.getElementById("sp").play();
+
+  function lect(y){ 
+    document.getElementById(y).play();
+  }
+  function stp(){
+    const elm = document.getElementsByTagName('audio');
+    for(i=0; i<elm.length ; i++){
+      elm[i].stop();
+    }
   }
