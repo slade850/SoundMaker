3064198f4c3920a2aa0584761d8fb4564d033e5f
Julien <julien.bardin81@gmail.com>
2019-05-16 09:44:25 +0200

    demarrage

---
+++ script.js
@@ -0,0 +1,46 @@
+function main() {
+    addListeners();
+  }
+  addEventListener('load',main);
+  
+  function showInfo() {
+    removeListeners();
+    
+    var modal = document.getElementsByClassName('modal')[0];
+        modal.style.display = 'block';
+    var close = modal.getElementsByClassName('close')[0];
+        close.addEventListener('click',hideInfo);
+    document.getElementById('overlay').addEventListener('click',hideInfo);
+  }
+  
+  function hideInfo() {
+    var boxes = document.querySelectorAll('.modal');
+    for (var i=0;i<boxes.length;i++) {
+      boxes[i].style.display = 'none';
+    }
+    addListeners();
+  }
+  
+  function addListeners() {
+    var boxes = document.querySelectorAll('.card');
+    for (var i=0;i<boxes.length;i++) {
+      boxes[i].addEventListener('click',showInfo.bind(boxes[i]));
+    }
+    handleOverlay('none');
+  }
+  
+  function removeListeners() {
+    var boxes = document.querySelectorAll('.card');
+    for (var i=0;i<boxes.length;i++) {
+      boxes[i].removeEventListener('click',showInfo);
+    }
+    handleOverlay('block');
+  }
+  
+  function handleOverlay(status) {
+    var overlay = document.getElementById('overlay');
+        overlay.style.display = status;
+  }
+  function lect(){
+  document.getElementById("sp").play();
+  }
